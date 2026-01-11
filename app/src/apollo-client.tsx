48f510484096063a055c04bbe2fd78ee1b14af93
Jieming Liu <jieming.liu@recursion.com>
2026-01-11 14:08:43 +0000

    feat: Add containerisation and enable docker compose

--- app/src/apollo-client.tsx
+++ app/src/apollo-client.tsx
@@ -1,8 +1,15 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 import { HttpLink } from '@apollo/client/link/http'
 
+const getBaseUrl = () => {
+    const protocol = window.location.protocol
+    const hostname = window.location.hostname
+    const port = import.meta.env.VITE_BACKEND_API_PORT || '8000'
+    return `${protocol}//${hostname}:${port}`
+}
+
 const httpLink = new HttpLink({
-    uri: 'http://localhost:8000/watch-cities/',
+    uri: `${getBaseUrl()}/watch-cities/`,
 })
 
 export const apolloClient = new ApolloClient({
